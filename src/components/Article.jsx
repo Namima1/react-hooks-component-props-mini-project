2b3cf4f5408bd9c88ab17f90fecade2183c82663
Namima Islam <namimaislam308@gmail.com>
2024-10-15 13:17:20 -0400

    completed lab

--- src/components/Article.jsx
+++ src/components/Article.jsx
@@ -1,12 +1,12 @@
 import React from "react";
 
-function Article({ title, date = "Janaury 1, 1970", preview }){
+function Article({ title, date = "January 1, 1970", preview }){
     return(
         <div>
         <article>
-            <h3>{title} "title"</h3>
-            <small>{date} "date"</small>
-            <p>{preview} "preview"</p>
+            <h3>{title}</h3>
+            <small>{date}</small>
+            <p>{preview}</p>
             
         </article>
         </div>
